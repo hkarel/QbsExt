427b38fd0c65ac8a75a047490bbd005f588f06b0
Pavel Karelin <hkarel@yandex.ru>
2017-01-17 13:06:54 +0300

    gccutl.js : Добавлена функция compilerLibraryPath()
    Функция возвращает директорию расположения библиотеки libstdc++
    для с++ компилятора указанного в параметре cppCompilerPath.

--- imports/GccUtl/gccutl.js
+++ imports/GccUtl/gccutl.js
@@ -1,6 +1,18 @@
 var File = loadExtension("qbs.File");
+var Process = loadExtension("qbs.Process");
 
 function ar(toolchainPathPrefix)
 {
     return File.exists(toolchainPathPrefix + "/gcc-ar") ? "gcc-ar" : "ar";
+}
+
+// Функция возвращает директорию расположения библиотеки libstdc++
+// для с++ компилятора указанного в параметре cppCompilerPath.
+function compilerLibraryPath(cppCompilerPath)
+{
+    var libPath = "";
+    var process = new Process();
+    if (process.exec(cppCompilerPath, ["--print-file-name=libstdc++.a"], false) === 0)
+        libPath = process.readLine().trim();
+    return libPath.replace("/libstdc++.a", "");
 }
