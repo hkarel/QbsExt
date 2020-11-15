6aeaa6e344786846b3728cdc537e95df841afcab
Pavel Karelin <hkarel@yandex.ru>
2020-11-15 19:07:10 +0300

    gccutl : Удалена функция compilerLibraryPath()
    Аналогичный функционал реализован в модуле cppstdlib

--- imports/GccUtl/gccutl.js
+++ imports/GccUtl/gccutl.js
@@ -1,23 +1,6 @@
 var File = require("qbs.File");
-var Process = require("qbs.Process");
 
 function ar(toolchainPathPrefix)
 {
     return File.exists(toolchainPathPrefix + "/gcc-ar") ? "gcc-ar" : "ar";
-}
-
-// Функция возвращает директорию расположения библиотеки libstdc++
-// для с++ компилятора указанного в параметре cppCompilerPath.
-function compilerLibraryPath(cppCompilerPath)
-{
-    var libPath = "";
-    var process = new Process();
-    try {
-        if (process.exec(cppCompilerPath, ["--print-file-name=libstdc++.a"], false) === 0)
-            libPath = process.readLine().trim();
-        return libPath.replace("/libstdc++.a", "");
-    }
-    finally {
-        process.close();
-    }
 }
